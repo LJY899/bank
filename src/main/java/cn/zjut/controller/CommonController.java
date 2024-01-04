424d5c38ace3deedb3b48abfedeb253acf9e0686
WJH <1538633425@qq.com>
2024-01-04 23:18:49 +0800

    文件上传下载

---
+++ src/main/java/cn/zjut/controller/CommonController.java
@@ -0,0 +1,83 @@
+package cn.zjut.controller;
+
+import cn.zjut.common.R;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/common")
+@Slf4j
+public class CommonController {
+    @org.springframework.beans.factory.annotation.Value("${bank.path}")
+    private String basePath;
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file){
+        log.info(file.toString());
+        //原始文件名
+        String originalFilename= file.getOriginalFilename();
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        //使用uuid重新生成文件名
+        String filename = UUID.randomUUID().toString()+suffix;
+
+        //创建一个目录
+        File dir=new File(basePath);
+        //判断目录是否存在
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+
+        try {
+            file.transferTo(new File(basePath+filename));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return R.success(filename);
+    }
+
+    /**
+     * 文件下载
+     * @param name
+     * @param response
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+        //输入流，读取文件内容
+        try {
+            FileInputStream fileInputStream=new FileInputStream(new File(basePath+name));
+            //输出流，协会浏览器
+            ServletOutputStream outputStream=response.getOutputStream();
+            response.setContentType("image/jpeg");
+            int len=0;
+            byte[] bytes=new byte[1024];
+            while((len = fileInputStream.read(bytes)) != -1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            outputStream.close();
+            fileInputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
