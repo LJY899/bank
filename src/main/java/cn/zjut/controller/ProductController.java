667a91059bbf5a590c7d1b19ab3ce5a8d37f00da
Charon <xzw371481@outlook.com>
2024-01-05 18:07:43 +0800

    修复误删bug

--- src/main/java/cn/zjut/controller/ProductController.java
+++ src/main/java/cn/zjut/controller/ProductController.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+
 @Slf4j
 @RestController
 @RequestMapping("/product")
