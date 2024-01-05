a486c40b2a2af06a17ec658583ee33ca7dd4eaf3
LiuJY <127284382+LJY899@users.noreply.github.com>
2024-01-05 18:48:47 +0800

    Revert "Merge pull request #1 from LJY899/addproduct"
    This reverts commit 0b51333518629eb5562d24d3828442efa58b32c6, reversing
    changes made to e3a88d6789d78c284fcc3bd7046ac23c8b87d026.

--- src/main/java/cn/zjut/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package cn.zjut.service;
-
-import cn.zjut.entity.Product;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface ProductService extends IService<Product> {
-}
